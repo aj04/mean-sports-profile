92a0c22e4be4217d757a2f915d2d31eafe03f5f6
ajay <ajay_dubey@hotmail.com>
2017-09-24 16:12:50 -0400

    Deploying to Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
-// to use the Native Promise make mongoose promise equals to native global Promise
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/SportsProfile');
+mongoose.promise = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/SportsProfile', {
+    useMongoClient: true,
+});
 
 module.exports = {mongoose};
